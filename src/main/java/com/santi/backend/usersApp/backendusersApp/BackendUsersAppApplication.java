39bf56f2bfdc0d05fd360fcf316fbe281f34d6f5
santi <santibrasca02@gmail.com>
2024-05-12 16:28:18 -0300

    Base files

---
+++ src/main/java/com/santi/backend/usersApp/backendusersApp/BackendUsersAppApplication.java
@@ -0,0 +1,13 @@
+package com.santi.backend.usersApp.backendusersApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUsersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUsersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santi/backend/usersApp/backendusersApp/BackendUsersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santi.backend.usersApp.backendusersApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
