aec5594734e2704e3d8a130928101abc35e2a416
santi <santibrasca02@gmail.com>
2024-05-13 00:56:35 -0300

    JwtAuthenticationFilter and add it into SpringSecurityConfig

--- src/main/java/com/santi/backend/usersApp/backendusersApp/auth/SpringSecurityConfig.java
+++ src/main/java/com/santi/backend/usersApp/backendusersApp/auth/SpringSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.santi.backend.usersApp.backendusersApp.auth;
 
+import com.santi.backend.usersApp.backendusersApp.auth.filters.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,11 +15,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SpringSecurityConfig {
 
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authz -> authz
         .requestMatchers(HttpMethod.GET, "/users").permitAll()
         .anyRequest().authenticated())
+        .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
         .csrf(AbstractHttpConfigurer::disable)//se desactiva CSRF en API Rest (sirve sólo para server-side rendering)
         .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //no maneja estados porque se usa el JWT
         .build();
---
+++ src/main/java/com/santi/backend/usersApp/backendusersApp/auth/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,80 @@
+package com.santi.backend.usersApp.backendusersApp.auth.filters;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.santi.backend.usersApp.backendusersApp.models.entities.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    //attemptAuthentication toma los datos del login (ya tiene implementada la ruta /login)
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+
+        User user = null;
+        String username = null;
+        String password = null;
+
+        try {
+            user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            username = user.getUsername();
+            password = user.getPassword();
+
+            logger.info("Username from InputStream request (raw) " + username);
+            logger.info("Password from InputStream request (raw) " + password);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+        return authenticationManager.authenticate(authToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String username = ((org.springframework.security.core.userdetails.User) authResult.getPrincipal())
+                .getUsername();
+        String originalInput = "algun_token_con_alguna_frase_secreta." + username;
+        String token = Base64.getEncoder().encodeToString(originalInput.getBytes());
+
+        response.addHeader("Authorization", "Bearer " + token);
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("token", token);
+        body.put("message", String.format("Hello %s, You've logged in successfully!", username));
+        body.put("username", username);
+        response.getWriter()
+                .write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(200);
+        response.setContentType("appplication/json");
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+         Map<String, Object> body = new HashMap<>();
+         body.put("message", "Authentication error. Wrong username or password!");
+         body.put("error", failed.getMessage());
+
+         response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+         response.setStatus(401);
+         response.setContentType("application/json");
+    }
+}
